baa412417a1f5532de913357957e87423ee9763d
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-06 13:32:57 +0700

    feat: implement sidebar state management for mobile responsiveness

--- src/app/(main)/[username]/layout.tsx
+++ src/app/(main)/[username]/layout.tsx
@@ -4,7 +4,9 @@ import { AppSidebar } from "@/components/app-sidebar";
 import DocumentHeader from "@/components/documents/header";
 import { LoadingSpinner } from "@/components/spinner";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { useConvexAuth } from "convex/react";
+import { useEffect, useState } from "react";
 
 export default function UsernameLayout({
   children,
@@ -12,6 +14,15 @@ export default function UsernameLayout({
   children: React.ReactNode;
 }) {
   const { isLoading } = useConvexAuth();
+  const isMobile = useIsMobile();
+
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  useEffect(() => {
+    if (isMobile) {
+      setSidebarOpen(false);
+    }
+  }, [isMobile]);
 
   if (isLoading) {
     return (
@@ -22,7 +33,7 @@ export default function UsernameLayout({
   }
 
   return (
-    <SidebarProvider>
+    <SidebarProvider open={sidebarOpen} onOpenChange={setSidebarOpen}>
       <AppSidebar />
       <main className="bg-secondary/20 grow">
         <DocumentHeader />
