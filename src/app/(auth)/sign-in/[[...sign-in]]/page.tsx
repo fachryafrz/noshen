a41ad209e3f7d03a155bb9a4f0cb4c25f0a9135c
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-06-07 14:21:13 +0700

    fix: update sign-in page layout to use min-h-dvh for better responsiveness

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="grid h-dvh place-content-center">
+    <div className="grid min-h-dvh place-content-center">
       <SignIn />
     </div>
   );
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="grid h-dvh place-content-center">
+    <div className="grid min-h-dvh place-content-center">
       <SignUp />
     </div>
   );
